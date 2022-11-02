52df79886b480f7902cd84e759de6db1a5768184
torstenSan <torsten@ama-mind.com>
2022-11-02 13:32:06 +0100

    inital commit

---
+++ src/main/java/com/ama_mind/ama_mind_app_DB/AmaMindAppDbApplication.java
@@ -0,0 +1,13 @@
+package com.ama_mind.ama_mind_app_DB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmaMindAppDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmaMindAppDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ama_mind/ama_mind_app_DB/AmaMindAppDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ama_mind.ama_mind_app_DB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmaMindAppDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
